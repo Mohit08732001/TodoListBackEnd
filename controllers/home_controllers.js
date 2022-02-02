ab5b473fc399232c7566fd6508c2176f58f9ef1b
Mohit Gupta <mohit0873.cse19@chitkara.edu.in>
2022-02-02 22:51:08 +0530

    Added controller and a router

---
+++ controllers/home_controllers.js
@@ -0,0 +1,5 @@
+module.exports.home = function(req,res){
+    return res.end('<h1>Controller is available</h1>');
+} 
+
+//module.exports.ActionName = function(req,res)
--- index.js
+++ index.js
@@ -2,6 +2,9 @@ const express=require('express');
 const app=express();
 const PORT=8000;
 
+// use express router
+app.use('/',require('./routes'));
+
 app.listen(PORT,function(err){
     if(err){
         console.log(`Error : ${err}`);
